e0caee32a2838f3f99ca33cbe661b3dc39f77a50
xithan <philipp.kuehl@gmail.com>
2014-12-06 08:35:38 +0100

    update notify_airbrake.rb

--- airbrake/set/all/notify_airbrake.rb
+++ airbrake/set/all/notify_airbrake.rb
@@ -1,3 +1,3 @@
 event :notify_airbrake, :after=>:notable_exception_raised do
-  controller.send :notify_airbrake, Card::Error.current if Airbrake.configuration.api_key
+  Env[:controller].send :notify_airbrake, Card::Error.current if Airbrake.configuration.api_key
 end
