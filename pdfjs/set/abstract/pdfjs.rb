0e920277737852864ef6f8f2e71cedc015c93b7a
Ethan McCutchen <ethan@grasscommons.org>
2017-01-24 15:57:54 -0500

    fix viewer urls

--- pdfjs/set/abstract/pdfjs.rb
+++ pdfjs/set/abstract/pdfjs.rb
@@ -1,20 +1,22 @@
 format :html do
   def pdf?
-    card.respond_to?(:file) && (mime = card.file.content_type) && mime == "application/pdf"
+    card.respond_to?(:file) && card.file.content_type == "application/pdf"
   end
 
   def default_pdfjs_iframe_args args
     args[:pdf_url] ||= card.file.url if card.respond_to?(:file)
-    args[:viewer_path] ||= "pdfjs/web/viewer.html"
+    args[:viewer_path] ||= "/pdfjs/web/viewer.html"
     args[:viewer_path] << "?file=#{args[:pdf_url]}" if args[:pdf_url]
   end
 
   view :pdfjs_iframe, cache: :never do |args|
     <<-HTML
-      <iframe style="width: 100%" id="source-preview-iframe" class="pdfjs-iframe"
+      <iframe style="width: 100%"
+              id="source-preview-iframe" class="pdfjs-iframe"
               src= #{args[:viewer_path]}
               security="restricted"
-              sandbox="allow-same-origin allow-scripts allow-forms allow-modals allow-top-navigation"
+              sandbox="allow-same-origin allow-scripts allow-forms allow-modals
+                       allow-top-navigation"
               allowfullscreen>
       </iframe>
     HTML
