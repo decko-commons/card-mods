ca0acbc8ad549a92c2406c4f1aef3801adc1553b
xithan <philipp.kuehl@gmail.com>
2017-08-31 21:07:07 +0200

    upgrade airbrake from v4 to v6

--- airbrake/config/initializers/airbrake.rb
+++ airbrake/config/initializers/airbrake.rb
@@ -1,11 +1,12 @@
 # -*- encoding : utf-8 -*-
 require 'airbrake'
 
-filename = File.join Decko.root, 'config/airbrake.key'
+filename = File.join Decko.root, 'config/airbrake.yml'
 if File.exists? filename or File.symlink? filename
-  key = File.read( filename ).strip
+  ab_config  = YAML.load_file(filename).with_indifferent_access
   Airbrake.configure do |config|
-    Rails.logger.info "setting up airbrake with #{key}"
-    config.api_key = key
+    Rails.logger.info "setting up airbrake"
+    config.project_key = ab_config[:project_key]
+    config.project_id = ab_config[:project_id]
   end
 end
