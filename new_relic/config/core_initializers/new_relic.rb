d91ad75e8853a2dcab02f732dbbf0fa5ec3e2633
xithan <philipp.kuehl@gmail.com>
2018-01-15 13:00:44 +0100

    trying to new relic event tracer to work

--- new_relic/config/core_initializers/new_relic.rb
+++ new_relic/config/core_initializers/new_relic.rb
@@ -1,12 +1,15 @@
 
 ActiveSupport.on_load :card do
+  require 'new_relic/agent/method_tracer'
+
   class Card
+    include ::NewRelic::Agent::MethodTracer
+
     module Set
       module Event
-        include ::NewRelic::Agent::MethodTracer
-
         def define_final_method event, method_name, &method
           class_eval do
+            include ::NewRelic::Agent::MethodTracer
             define_method method_name, &method
             add_method_tracer method_name, "event/#{event}"
           end
