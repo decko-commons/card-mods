8fb0fcdbba8c45ca1724e59c1d98292a44aeb3e2
Ethan McCutchen <ethan@grasscommons.org>
2018-06-08 10:04:58 -0400

    instrumentation for Render, Parser, Query, View

--- new_relic/config/core_initializers/new_relic.rb
+++ new_relic/config/core_initializers/new_relic.rb
@@ -7,12 +7,10 @@ ActiveSupport.on_load :card do
 
     module Set
       module Event
-        extend ::NewRelic::Agent::MethodTracer
         def define_simple_method event, method_name, &method
           class_eval do
-            #include
             define_method method_name, &method
-            add_method_tracer method_name, "event/#{event}"
+            Card.add_method_tracer method_name, "event/#{event}"
           end
         end
       end
@@ -21,8 +19,26 @@ ActiveSupport.on_load :card do
     class Format
       module Render
         include ::NewRelic::Agent::MethodTracer
-        add_method_tracer! :render!
+        add_method_tracer :render!, "Format/render"
       end
+    end
+
+    class Content
+      class Parser
+        include ::NewRelic::Agent::MethodTracer
+        add_method_tracer :parse, "Content/parse"
+      end
+    end
+
+    module Query
+      include ::NewRelic::Agent::MethodTracer
+      add_method_tracer :new, "Query/new"
+    end
+
+    class View
+      include ::NewRelic::Agent::MethodTracer
+      add_method_tracer :initialize, "View/initialize"
+      add_method_tracer :process, "View/process"
     end
   end
 end
--- new_relic/set/all/new_relic.rb
+++ new_relic/set/all/new_relic.rb
@@ -43,13 +43,6 @@ def add_custom_card_attributes
   )
 end
 
-format do
-  include ::NewRelic::Agent::MethodTracer
-  def render!
-
-  end
-end
-
 
 ::Card::Set::Event::IntegrateWithDelayJob.after_perform do |job|
   ActManager.contextualize_delayed_event *job.arguments[0..3] do
