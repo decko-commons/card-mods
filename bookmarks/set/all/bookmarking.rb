d1d6c2abc1989e24c3cac9ee28ac21a48a98e460
Ethan McCutchen <ethan@grasscommons.org>
2019-12-06 13:49:09 -0500

    bookmarking tweaks

--- bookmarks/set/all/bookmarking.rb
+++ bookmarks/set/all/bookmarking.rb
@@ -4,12 +4,22 @@ def current_bookmark_list
   @current_bookmark_list ||= Auth.current.bookmarks_card
 end
 
-def current_bookmarks_of_type type
-  return [] unless Auth.can_bookmark?
-
-  current_bookmark_list.item_cards_of_type type
+def current_bookmark_ids
+  bookmark_items { current_bookmark_list.item_ids }
 end
 
-def current_bookmarks_names_of_type type
+def current_bookmarks_of_type type
+  bookmark_items { current_bookmark_list.item_cards_of_type type }
+end
+
+def current_bookmark_names_of_type type
   current_bookmarks_of_type(type).map(&:name)
+end
+
+def bookmark_items
+  Auth.can_bookmark? ? yield : []
+end
+
+format do
+  delegate :current_bookmarks_of_type, :current_bookmark_names_of_type, to: :card
 end
