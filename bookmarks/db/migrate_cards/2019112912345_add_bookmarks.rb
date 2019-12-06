fa77d5f88b5a118fb0633f75454ca60b4b3a0174
Ethan McCutchen <ethan@grasscommons.org>
2019-12-06 14:51:27 -0500

    repair bookmark migration

--- bookmarks/db/migrate_cards/2019112912345_add_bookmarks.rb
+++ bookmarks/db/migrate_cards/2019112912345_add_bookmarks.rb
@@ -6,6 +6,13 @@ class AddBookmarks < Card::Migration
     ensure_code_card "Bookmarkers"
     Card.search right: :upvotes do |vote_card|
       vote_card.update! name: Card::Name[vote_card.name.left, :bookmarks]
+      vote_card.standardize_items # convert to id pointer
+      vote_card.save!
+
+      vote_card.item_cards.each do |markee|
+        next unless (marker_search = markee.try(:bookmarkers_card))
+        marker_search.cached_count
+      end
     end
     Card[:metric_voter].update! name: "Metric Bookmarker", codename: "metric_bookmarker"
   end
