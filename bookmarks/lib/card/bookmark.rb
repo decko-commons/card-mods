f6024228109df0fadeff84bdbab29c46b533df90
Ethan McCutchen <ethan@grasscommons.org>
2019-12-09 12:21:11 -0500

    bookmark api optimization

--- bookmarks/lib/card/bookmark.rb
+++ bookmarks/lib/card/bookmark.rb
@@ -11,12 +11,15 @@ class Card
       # @return Hash key is type_id, value is list of ids
       def current_bookmarks
         cache.fetch CURRENT_BOOKMARKS_KEY do
-          bookmark_list do
-            current_list_card.item_cards.each_with_object({}) do |item, hash|
-              hash[item.type_id] ||= []
-              hash[item.type_id] << item.id
-            end
-          end
+          ok? ? bookmark_hash_for_list_card(current_list_card) : {}
+        end
+      end
+
+      # @param list_card [Card] +bookmarks card for (eg) user
+      def bookmark_hash_for_list_card list_card
+        list_card.item_cards.each_with_object({}) do |item, hash|
+          hash[item.type_id] ||= []
+          hash[item.type_id] << item.id
         end
       end
 
@@ -30,8 +33,8 @@ class Card
         end
       end
 
-      def bookmark_list
-        ok? ? yield : []
+      def bookmark_hash
+        ok? ? yield : {}
       end
 
       def id_restriction bookmarked=true
@@ -49,6 +52,13 @@ class Card
       def clear
         cache.delete CURRENT_IDS_KEY
         cache.delete CURRENT_BOOKMARKS_KEY
+      end
+
+      def sort rel, join_field, sort_order
+        rel.joins(
+          "LEFT JOIN counts cts " \
+          "ON #{join_field} = cts.left_id AND cts.right_id = #{BookmarkersID}"
+        ).order "cts.value #{sort_order}"
       end
     end
   end
