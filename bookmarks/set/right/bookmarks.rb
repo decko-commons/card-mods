60e9bf03302a32557bd93bdcc6004a5397ccdead
Ethan McCutchen <ethan@grasscommons.org>
2019-12-10 09:16:50 -0500

    stop bookmark propagation

--- bookmarks/set/right/bookmarks.rb
+++ bookmarks/set/right/bookmarks.rb
@@ -1,5 +1,13 @@
 include Abstract::IdPointer
 
+def history?
+  false
+end
+
+def followable?
+  false
+end
+
 def item_cards_of_type type
   type_id = Card.fetch_id type
   item_cards.select { |i| i.type_id == type_id }
