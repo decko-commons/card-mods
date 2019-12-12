2148e8dca7f4a407c773a94058e615fc863329e4
Ethan McCutchen <ethan@grasscommons.org>
2019-12-12 16:21:34 -0500

    handle following "bookmarked" cards

---
+++ bookmarks/set/self/bookmarked.rb
@@ -0,0 +1,20 @@
+include_set Abstract::FollowOption
+
+restrictive_follow_opts :position=>3
+
+follower_candidate_ids do |card|
+  Card.search({ right_plus: [:bookmarks, { refer_to: card.name }], return: :id },
+              "bookmarked follower candidate ids for #{card.name}")
+end
+
+def title
+  'Following content you bookmarked'
+end
+
+def label
+  'follow if I bookmarked'
+end
+
+def description set_card
+  "#{set_card.follow_label} I bookmarked"
+end
