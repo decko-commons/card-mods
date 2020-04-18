5748fa7a1cdf40b1c09b07524168d26fa8d88e5a
Ethan McCutchen <ethan@grasscommons.org>
2020-04-18 15:57:34 -0400

    import item helper method

--- csv_import/lib/import_item/normalizer.rb
+++ csv_import/lib/import_item/normalizer.rb
@@ -1,5 +1,6 @@
 class ImportItem
-  # common methods to be used to normalize values
+  # common methods to be used to normalize values#
+  # TODO: rename this to HelperMethods
   module Normalizer
     def comma_list_to_pointer str, comma=","
       str.split(comma).map(&:strip).to_pointer_content
@@ -7,6 +8,10 @@ class ImportItem
 
     def to_html value
       value.gsub "\n", "<br>"
+    end
+
+    def select_present hash
+      hash.select { |_k, v| v.present? }
     end
   end
 end
