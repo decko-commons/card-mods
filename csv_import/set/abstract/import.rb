22ad692a72b2d3b58a4f696b0cb9459fbb0f53b4
Ethan McCutchen <ethan@grasscommons.org>
2020-05-12 22:39:31 -0400

    start import reorganization

--- csv_import/set/abstract/import.rb
+++ csv_import/set/abstract/import.rb
@@ -39,27 +39,3 @@ end
 def status
   @status ||= import_status_card.status
 end
-
-
-format :html do
-  before :new do
-    voo.help = help_text
-    voo.show! :help
-  end
-
-  before :edit do
-    voo.help = help_text
-    voo.show! :help
-  end
-
-  def help_text
-    headers = card.import_item_class.headers
-    "expected import item format: #{headers.join ', '}"
-  end
-
-  def download_link
-    handle_source do |source|
-      %(<a href="#{source}" rel="nofollow">Download File</a><br />)
-    end.html_safe
-  end
-end
