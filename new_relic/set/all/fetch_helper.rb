3cdd11400511bb2903b1d9a7a4b4be2f59a1d88a
Ethan McCutchen <ethan@grasscommons.org>
2020-06-04 10:28:16 -0400

    some new relic tracers off (for now)

--- new_relic/set/all/fetch_helper.rb
+++ new_relic/set/all/fetch_helper.rb
@@ -1,6 +1,6 @@
 module ClassMethods
   # add_method_tracer :retrieve_from_cache, "Custom/Card/retrieve_from_cache"
-  add_method_tracer :normalize_fetch_args, "Custom/Card/normalize_fetch_args"
-  add_method_tracer :retrieve_or_new, "Custom/Card/retrieve_or_new"
-  add_method_tracer :standard_fetch_results, "Custom/Card/standard_fetch_results"
+  # add_method_tracer :normalize_fetch_args, "Custom/Card/normalize_fetch_args"
+  # add_method_tracer :retrieve_or_new, "Custom/Card/retrieve_or_new"
+  # add_method_tracer :standard_fetch_results, "Custom/Card/standard_fetch_results"
 end
