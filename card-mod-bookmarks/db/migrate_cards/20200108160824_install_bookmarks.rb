a1867f178aa39c90e8468c1f806d9047796be4ad
Ethan McCutchen <ethan@decko.org>
2021-10-21 15:36:57 -0400

    moving mods from wikirate to card-mods repo

--- card-mod-bookmarks/db/migrate_cards/20200108160824_install_bookmarks.rb
+++
@@ -1,8 +0,0 @@
-# -*- encoding : utf-8 -*-
-
-class InstallBookmarks < Cardio::Migration
-  def up
-    ensure_code_card "Bookmarks"
-    ensure_code_card "Bookmarkers"
-  end
-end
---
+++ card-mod-tabs/card-mod-tabs.gemspec
@@ -0,0 +1,9 @@
+# -*- encoding : utf-8 -*-
+
+require "../card_mod_gem"
+
+CardModGem.mod "tabs" do |s, _d|
+  s.version = "0.1"
+  s.summary = "tabs"
+  s.description = ""
+end
