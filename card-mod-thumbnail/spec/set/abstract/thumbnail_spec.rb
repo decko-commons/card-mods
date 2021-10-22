b64e0e1d0d6e1f32d3ad842e614802c35530f896
Ethan McCutchen <ethan@decko.org>
2021-10-22 00:47:31 -0400

    moved out wikirate-specific thumbnail spec

--- card-mod-thumbnail/spec/set/abstract/thumbnail_spec.rb
+++ card-mod-thumbnail/spec/set/abstract/thumbnail_spec.rb
@@ -1,8 +1,4 @@
 RSpec.describe Card::Set::Abstract::Thumbnail do
-  # def card_subject
-  #   Card["Death Star"]
-  # end
-
   specify "view thumbnail" do
     expect_view(:thumbnail).to have_tag("div.thumbnail") do
       with_tag "div.image-box.small" do
@@ -33,16 +29,6 @@ RSpec.describe Card::Set::Abstract::Thumbnail do
       with_tag "div.thumbnail-text" do
         with_unlinked "thumbnail-title"
         with_tag "div.thumbnail-subtitle"
-      end
-    end
-  end
-
-  specify "view thumbnail_image" do
-    expect_view(:thumbnail).to have_tag("div.image-box") do
-      with_tag "div.image-box.small" do
-        with_tag "a.known-card" do
-          with_tag "i.fa-building"
-        end
       end
     end
   end
