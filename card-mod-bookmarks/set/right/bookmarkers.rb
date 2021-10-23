8059178e83a64171b1715d5571ae54332f7cd146
Ethan McCutchen <ethan@decko.org>
2021-10-23 17:59:47 -0400

    wikirate -> card-mods: bookmarkers

--- card-mod-bookmarks/set/right/bookmarkers.rb
+++ card-mod-bookmarks/set/right/bookmarkers.rb
@@ -1,3 +1,8 @@
+# cache # of users who have bookmarked this metric/topic/whatever(=left)
+include_set Abstract::TaggedByCachedCount, type_to_count: :user,
+                                           tag_pointer: :bookmarks,
+                                           count_trait: :bookmarkers
+
 def active?
   left&.currently_bookmarked?
 end
