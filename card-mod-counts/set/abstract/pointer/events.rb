2bbe913ab7bd38bb5f0af2bfceb30a5172167036
Ethan McCutchen <ethan@decko.org>
2021-12-15 11:44:38 -0500

    stash changed_item_names

---
+++ card-mod-counts/set/abstract/pointer/events.rb
@@ -0,0 +1,18 @@
+# Abstract::ListRefCachedCount needs to know which items have been changed.
+# But the standard ActiveModel dirty/mutations mechanism for this is not serializable
+# and therefore cannot be thus preserved for delayed jobs (when the cached counting
+# now takes place.)
+#
+# In the long term it would be preferable to have a more general solution, but for
+# now we are just stashing
+
+Card.action_specific_attributes << :changed_item_names
+
+# make these available in delayed jobs
+event :stash_changed_item_names, :after_integrate do
+  @changed_item_names = changed_item_names
+end
+
+def changed_item_names
+  @changed_item_names || dropped_item_names + added_item_names
+end
