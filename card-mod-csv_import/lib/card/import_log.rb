17053d456c6ef8425d0958a381d493f3a1f02cc0
Ethan McCutchen <ethan@decko.org>
2022-02-06 22:41:06 -0500

    import_log whitespace

--- card-mod-csv_import/lib/card/import_log.rb
+++ card-mod-csv_import/lib/card/import_log.rb
@@ -1,7 +1,7 @@
 class Card
   class ImportLog
     LOG_FILE = Cardio.paths["log"].existent.first&.sub(/\.log$/, "-import.log")
-    
+
     class << self
       attr_accessor :logger
       delegate :debug, :info, :warn, :error, :fatal, to: :logger
