0b853dbd519784085c6e8f75071f7472a9281f32
Ethan McCutchen <ethan@decko.org>
2022-05-03 15:17:51 -0400

    move bookmark filters into bookmarks mod

---
+++ card-mod-bookmarks/set/abstract/bookmark_filters.rb
@@ -0,0 +1,14 @@
+def filter_keys
+  [:bookmark]
+end
+
+format :html do
+  def filter_bookmark_type
+    :select
+  end
+
+  def filter_bookmark_options
+    { "I bookmarked" => :bookmark,
+      "I did NOT bookmark" => :nobookmark }
+  end
+end
