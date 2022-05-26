6ab33b0d6e20aef31cb6a08e77f5123fba644c24
Ethan McCutchen <ethan@decko.org>
2022-05-26 17:34:46 -0400

    script calls and filter input cleanup

--- card-mod-prosemirror_editor/set/all/prosemirror_editor.rb
+++ card-mod-prosemirror_editor/set/all/prosemirror_editor.rb
@@ -1,7 +1,11 @@
 basket[:list_input_options] << "prosemirror editor"
-basket[:script_config][:prose_mirror] = "setProseMirrorConfig"
+basket[:script_calls]["setProseMirrorConfig"] = :prosemirror_config
 
 format :html do
+  def prosemirror_config
+    Card::Rule.global_setting :prose_mirror
+  end
+
   def prosemirror_editor_input
     wrap_with :div, id: unique_id, class: "prosemirror-editor" do
       hidden_field :content, class: "d0-card-content", value: card.content
