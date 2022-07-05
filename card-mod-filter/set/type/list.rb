7f50cf65e75a2177f355eb4c1a06a61ee8adbc6b
Ethan McCutchen <ethan@decko.org>
2022-07-05 18:55:25 -0400

    filter mod docs

--- card-mod-filter/set/type/list.rb
+++ card-mod-filter/set/type/list.rb
@@ -1,4 +1,4 @@
 def input_type_content_options
-  # TODO: need to make super work here somehow
+  # TODO: need to make super work here somehow or refactor around baskets
   %w[multiselect checkbox list] << "filtered list"
 end
