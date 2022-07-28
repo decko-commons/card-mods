c4ced7ffecf84dfa84cf4a5fcb1b8ec8086c3cc4
Ethan McCutchen <ethan@decko.org>
2022-07-27 22:35:05 -0400

    minor import suggestions refactor

--- card-mod-csv_import/set/abstract/search.rb
+++ card-mod-csv_import/set/abstract/search.rb
@@ -1,10 +1,11 @@
 format :html do
   view :import_suggestions do
-    output(import_suggestions_search.map { |item| import_suggestion item })
+    results = Env.with_params(limit: 3) { import_suggestions_search }
+    output(results.map { |item| import_suggestion item })
   end
 
   def import_suggestions_search
-    Env.with_params(limit: 3) { search_with_params }
+    search_with_params
   end
 
   def import_suggestion item
