2c969a90001e964de8c7f7191682c24a1cd83d20
Christina Schweipert <44006691+cschweipert@users.noreply.github.com>
2023-09-15 15:09:35 -0600

    code climate fix

--- card-mod-blog/spec/set/type/blog_spec.rb
+++ card-mod-blog/spec/set/type/blog_spec.rb
@@ -1,12 +1,12 @@
 RSpec.describe Card::Set::Type::Blog do
   context "when getting and setting attributes" do
       def card_subject
-        Card.create! type: "Blog", name: "My Blog", fields: { 
-          description: "description", 
-          date: "DD/MM/YY" 
+        Card.create! type: "Blog", name: "My Blog", fields: {
+          description: "description",
+          date: "DD/MM/YY"
         }
-  end
+      end
 
       check_views_for_errors
-    end
+  end
 end
