ac0a7ad06a94ae0cbac130758b5dfe2b40a77e8b
Christina Schweipert <44006691+cschweipert@users.noreply.github.com>
2023-09-19 15:43:05 -0600

    add test for blog description

--- card-mod-blog/spec/set/type/blog_spec.rb
+++ card-mod-blog/spec/set/type/blog_spec.rb
@@ -2,21 +2,21 @@ RSpec.describe Card::Set::Type::Blog do
   context "when getting and setting attributes" do
     def card_subject
       Card.create! type: "Blog", name: "My Blog", fields: {
-        description: "This is blog post description.",
-        date: "01/01/23"
+        description: "This is a blog post description.",
+        date: "DD/MM/YY"
       }
     end
-    
     check_views_for_errors
 
-    it "renders the correct description" do
-      blog_card = card_subject
-      expect(blog_card.description).to eq("This is blog post description.")
-    end
+    it "should get and set the description attribute" do
+      card = card_subject
 
-    it "renders the correct date" do
-      blog_card = card_subject
-      expect(blog_card.date).to eq("01/01/23")
+      expect(card.description).to eq("This is a blog post description.")
+
+      card.description = "This is a new description."
+      card.save!
+
+      expect(card.description).to eq("This is a new description.")
     end
   end
 end
