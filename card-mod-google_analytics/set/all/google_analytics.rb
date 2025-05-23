fd368354fc254a107f9ab9b871d45a6f0c210555
Ethan McCutchen <ethan@decko.org>
2025-05-22 20:34:26 -0400

    ga api tracker using faraday

--- card-mod-google_analytics/set/all/google_analytics.rb
+++ card-mod-google_analytics/set/all/google_analytics.rb
@@ -1,50 +1,8 @@
-require "faraday"
-
-event :track_page, before: :show_page, when: :track_page_from_server? do
-  # track_page!
-  puts tracker_event_paramaters
-end
-
-def track_page!
-  tracker&.pageview tracker_options
-end
 
 def google_analytics_keys
   @google_analytics_keys ||= Array.wrap(
     Card::Rule.global_setting(:google_analytics_key) || Card.config.google_analytics_key
   )
-end
-
-def tracker
-  nil
-  # tracker_key && ::Staccato.tracker(tracker_key) # , nil, ssl: true
-end
-
-# can have separate keys for web and API
-def tracker_key
-  Card.config.google_analytics_tracker_key || google_analytics_keys.first
-end
-
-def tracker_event_parameters
-  r = Env.controller.request
-  {
-    cardtype: type_name,
-    cardtype_id: type_id,
-    card_id: id,
-    card_name: name,
-    limit: Env.params[:limit],
-    api_key: Auth.api_keys.first,
-    query_string: r.query_string,
-    # status_code: "XXXX",
-    # error_message:,
-    # client:,
-    # response_time:,
-  }
-end
-
-# for override
-def track_page_from_server?
-  false
 end
 
 format :html do
