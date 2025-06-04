a5c5583a304fc564bac0ff585dd1d5d45d9ce718
Ethan McCutchen <ethan@decko.org>
2025-06-04 11:53:41 -0400

    refactor api tracker to use ActiveSupport notification subscription

---
+++ card-mod-google_analytics/lib/card/api_tracker.rb
@@ -0,0 +1,111 @@
+require "faraday"
+
+class Card
+  class ApiTracker
+    attr_reader :event
+
+    def initialize event
+      @event = event
+    end
+
+    def body
+      {
+        "client_id": client_id,
+        "events": [
+          {
+            "name": event_name,
+            "params": event_params
+          }
+        ]
+      }
+    end
+
+    def client_id
+      Digest::SHA256.hexdigest(Auth.current_id.to_s + self.class.config(:salt))
+    end
+
+    def payload
+      @event.payload
+    end
+
+    def event_name
+      action = payload[:action]
+      "api_data_#{action == 'read' ? 'fetch' : action}"
+    end
+
+    def event_params
+      {
+        status_code: payload[:status],
+        format: payload[:format],
+        path: payload[:path],
+        # query_params: request.query_string,
+        client: payload[:request].user_agent,
+        response_time: event.duration,
+        engagement_time_msec: 1
+      }.merge card_params, format_params
+    end
+
+    def card
+      Env.controller.card
+    end
+
+    def format
+      Env.controller.format
+    end
+
+    def card_params
+      card.api_tracker_event_params
+    end
+
+    def format_params
+      format.api_tracker_event_params
+    end
+
+    class << self
+      def track! event
+        tracker = new event
+        response = connection.post(path) do |req|
+          rbody = tracker.body
+          req.body = rbody.to_json
+          puts JSON.pretty_generate rbody if debug?
+        end
+        Rails.logger.debug response.body if debug?
+      end
+
+      def track event
+        track! event if track?
+      end
+
+      def config field
+        Cardio.config.send "google_analytics_api_tracker_#{field}"
+      end
+
+      private
+
+      def track?
+        Cardio.config.track_api_requests && Auth.api_request?
+      end
+
+      def debug?
+        config :debug
+      end
+
+      def connection
+        @connection ||= Faraday.new(
+          url: "https://www.google-analytics.com",
+          params: {
+            measurement_id: config(:measurement_id),
+            api_secret: config(:api_secret)
+          },
+          headers: { "Content-Type" => "application/json" }
+        ) do |faraday|
+          faraday.response :logger if debug?
+        end
+      end
+
+      def path
+        @path ||= debug? ? "debug/mp/collect" : "mp/collect"
+      end
+    end
+  end
+end
--- card-mod-google_analytics/set/all/google_analytics_api_tracker.rb
+++ card-mod-google_analytics/set/all/google_analytics_api_tracker.rb
@@ -1,97 +1,27 @@
-require "faraday"
-
-event :track_api_request, after: :show_page, when: :track_api_request? do
-  track_api_request!
-end
-# for override
-def track_api_request?
-  Cardio.config.track_api_requests && Auth.api_request?
-end
-
-def track_api_request!
-  # uri = URI("https://www.google-analytics.com/mp/collect")
-  # params = {
-  #   measurement_id: Cardio.config.google_analytics_api_tracker_measurement_id,
-  #   api_secret: Cardio.config.google_analytics_api_tracker_api_secret
-  # }
-  # uri.query = URI.encode_www_form(params)
-  #
-  # res = Net::HTTP.post_form uri,
-  #                           body:
-
-  response = api_tracker_connection.post(api_tracker_path) do |req|
-    req.body = api_tracker_body.to_json
-    # puts req.body
-  end
-  Rails.logger.debug response.body if Cardio.config.google_analytics_api_tracker_debug
-end
-
-private
-
-def api_tracker_client_id
-  Digest::SHA256.hexdigest(Auth.current_id.to_s +
-                           Cardio.config.google_analytics_api_tracker_salt)
-end
-
-def api_tracker_connection
-  @api_tracker_connection ||= Faraday.new(
-    url: "https://www.google-analytics.com",
-    params: {
-      measurement_id: Cardio.config.google_analytics_api_tracker_measurement_id,
-      api_secret: Cardio.config.google_analytics_api_tracker_api_secret
-    },
-    headers: { "Content-Type" => "application/json" }
-  ) do |faraday|
-    faraday.response :logger
-  end
-end
-
-def api_tracker_path
-  if Cardio.config.google_analytics_api_tracker_debug
-    "debug/mp/collect"
-  else
-    "mp/collect"
-  end
-end
-
-def api_tracker_body
-  {
-    "client_id": api_tracker_client_id,
-    "events": [
-      {
-        "name": "api_data_fetch",
-        "params": api_tracker_event_params
-      }
-    ]
-  }
-end
-
-def api_tracker_status_code
-  Env.controller.format.error_status || 200
-end
-
-def api_tracker_result_count
-  results = Env.controller.format.instance_variable_get("@search_with_params")
-  results.present? ? results.count : ""
-end
-
-def api_tracker_error_message
-  Env.controller.card.format(:text).error_messages.join "; "
-end
 
 def api_tracker_event_params
-  r = Env.controller.request
-
   {
     cardtype: type_name,
     cardtype_id: type_id,
-    card_id: id,
+    card_id: id || "",
     card_name: name,
-    status_code: api_tracker_status_code,
-    result_count: api_tracker_result_count,
-    error_message: api_tracker_error_message,
-    query_params: r.query_string,
-    client: r.user_agent,
-    engagement_time_msec: 1
+    error_message: api_tracker_error_message
   }
+end
+
+def api_tracker_error_message
+  format(:text).error_messages.join "; "
+end
+
+# def api_tracker_status_code
+#   Env.controller.format.error_status || 200
+# end
+format do
+  def api_tracker_event_params
+    { result_count: api_tracker_result_count }
+  end
+
+  def api_tracker_result_count
+    @search_with_params.present? ? @search_with_params.count : ""
+  end
 end
